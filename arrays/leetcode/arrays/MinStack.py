1359c98bcc09e912b53d3ccfa39ba4763ed82e2b
0utsights <outsightszs@outlook.com>
2025-12-04 22:51:54 -0500

    quick leetcode solution

---
+++ arrays/leetcode/arrays/MinStack.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val) # check the minimum value between the current value and the last value in minStack (if it exists)
+        self.minStack.append(val) # add the minimum value to the minStack
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+# everything is done in O(1) time and therefore in maximum efficiency 
