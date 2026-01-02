828de3de812bc1bcff230af6f7551363e2c871b1
0utsights <outsightszs@outlook.com>
2026-01-01 23:07:36 -0500

    annotated and reviewed previous leetcode problem

--- arrays/leetcode/LinkedLists/reverselinkedlist.py
+++ arrays/leetcode/LinkedLists/reverselinkedlist.py
@@ -8,13 +8,13 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         #recursive: T O(n), M O(n)
 
-        if not head:
-            return None
+        if not head: # if head is None return None
+            return None 
 
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
+        newHead = head # initialize newHead to current head
+        if head.next: # if there is next node
+            newHead = self.reverseList(head.next) # recursively call reverseList on next node and set newHead to result
+            head.next.next = head # set next node's next pointer to current head (reversing the link)
+        head.next = None # set current head's next pointer to None (to avoid cycle)
 
-        return newHead
+        return newHead # return newHead which is head of the reversed list
